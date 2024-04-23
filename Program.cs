ae613ca5179f5b2283851137b8352629b7f18cb1
Deepak Poly <119725894+Daddy2424@users.noreply.github.com>
2024-04-24 03:16:10 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Connect4_Final_Ptoject
@@ -10,110 +11,21 @@ namespace Connect4_Final_Ptoject
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Press any key : ");
             Console.ReadKey();
             ConsoleCenter("CONNECT FOUR");
-
-            Player player1 = new Player();
-            Player player2 = new Player();
             
 
-            Console.WriteLine(" ");
-            Console.WriteLine(" ");
-            ConsoleCenter("Enter Player 1 name : ");
-            player1.Name = Console.ReadLine();
-            player1.Turn = "X";
-            Console.WriteLine(" ");
-            ConsoleCenter("Enter Player 2 name : ");
-            player2.Name = Console.ReadLine();
-            player2.Turn = "O";
-
-            // Creating Gameboard
-            GameBoard gameBoard = new GameBoard(6, 7);
-            Controller controller = new Controller(gameBoard);
-
-            int exit = 0;
-            int i = 0;
-            int selectedCol;
-            do
-            {
-                Console.Clear();
-                // Player 1 turn
-                gameBoard.GameBoardDisplay();
-                Console.WriteLine(" ");
-
-                while (i == 0)
-                {
-                    Console.Write("Your turn {0} (Choose a number) : ", player1.Name);
-                    string input = Console.ReadLine();
-                    if (string.IsNullOrWhiteSpace(input))
-                    {
-                   
-                        Console.WriteLine("Please enter a valid number.");
-                        continue; 
-                    }
-                    if (int.TryParse(input, out selectedCol))
-                    {
-                        if (selectedCol >= 1 && selectedCol <= 7)
-                        {
-                            i = 1;
-                            Console.WriteLine(" ");
-                            controller.ChangeBoard(selectedCol, player1.Turn);
-                        }
-                        else
-                        {
-                            Console.WriteLine("Please enter a number between 1 and 7.");
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("Please enter a valid number.");
-                    }
-                }
-
-
-
-                Console.Clear();
-                // Player 2 turn
-                gameBoard.GameBoardDisplay();
-                Console.WriteLine(" ");
-                while (i == 1)
-                {
-                    Console.Write("Your turn {0} (Choose a number) : ", player2.Name);
-                    string input = Console.ReadLine();
-                    if (string.IsNullOrWhiteSpace(input))
-                    {
-                        Console.WriteLine("Please enter a valid number.");
-                        continue; 
-                    }
-                    if (int.TryParse(input, out selectedCol))
-                    {
-                        if (selectedCol >= 1 && selectedCol <= 7)
-                        {
-                            i = 0;
-                            Console.WriteLine(" ");
-                            controller.ChangeBoard(selectedCol, player2.Turn);
-                        }
-                        else
-                        {
-                            Console.WriteLine("Please enter a number between 1 and 7.");
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("Please enter a valid number.");
-                    }
-                }
-
-
-            } while (exit == 0);
-
+            // Setting controller for gameboard
+            Controller controller = new Controller();
+            
+            // menu display
+            Menu menu = new Menu();
+            menu.MenuDisplay(controller, menu); 
         }
 
 
-
-
-
+        // function for aligning items in center
         public static void ConsoleCenter(string text)
         {
 
@@ -123,6 +35,16 @@ namespace Connect4_Final_Ptoject
 
             Console.Write(string.Format("{0," + (padding + textWidth) + "}", text));
 
+        }
+
+        // function for typeWrite animation
+        public static void typeWrite(string text, int delay = 50)
+        {
+            foreach (char c in text)
+            {
+                Console.Write(c);
+                Thread.Sleep(delay);
+            }
         }
 
     }
