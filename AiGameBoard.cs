db349b49b3e1414d5311bb5e601eb02c70e491f5
Deepak Poly <119725894+Daddy2424@users.noreply.github.com>
2024-04-23 15:47:58 -0600

    Add files via upload

---
+++ AiGameBoard.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Connect4_Final_Ptoject
+{
+    internal class AiGameBoard : GameBoard
+    {
+
+        private int _rows;
+        private int _columns;
+        private string[,] _arr;
+
+        public AiGameBoard(int rows, int columns) : base(rows, columns)
+        {
+            _rows = rows;
+            _columns = columns;
+            _arr = new string[_rows, _columns];
+
+            for (int i = 0; i < _rows; i++)
+            {
+                for (int j = 0; j < _columns; j++)
+                {
+                    _arr[i, j] = "-";
+                }
+            }
+        }
+
+
+        public void AiPlayGame(Controller controller, Menu menu)
+        {
+            Console.Clear();
+            var originalMargin = (Console.WindowWidth - 6) / 2;
+            var leftMargin = (Console.WindowWidth - 21) / 2;
+            var centerMargin = (Console.WindowWidth - 31) / 2;
+            while (true)
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine(" ");
+                Program.ConsoleCenter("Enter Player 1 name : ");
+                string player1Name = Console.ReadLine();
+                Console.WriteLine(" ");
+                if (string.IsNullOrWhiteSpace(player1Name))
+                {
+                    Console.Clear();
+                    Program.ConsoleCenter("Please enter a valid name // ");
+                    Thread.Sleep(2000);
+                    Console.Clear();
+                }
+                else
+                {
+                    CreatePlayer(player1Name, "AI");
+                    break;
+                }
+            }
+            Console.Clear();
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Program.ConsoleCenter("Player 1 will be X and AI will be O ");
+            Console.WriteLine(" ");
+            Console.CursorLeft = originalMargin;
+            Program.typeWrite("....", 1000);
+            Thread.Sleep(1000);
+
+            int exit = 0;
+            int i = 0;
+            int selectedCol;
+            do
+            {
+                Console.Clear();
+                // Player 1 turn
+                controller.GameBoardDisplay();
+                // check draw
+                if (controller.IsFull())
+                {
+                    Console.Clear();
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.CursorLeft = leftMargin;
+                    Console.WriteLine("Its a DRAW guys ! play again //");
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    while (true)
+                    {
+                        Program.ConsoleCenter("Enter X to go to menu : ");
+                        string input = Console.ReadLine();
+                        if (input.ToLower() == "x")
+                        {
+                            menu.MenuDisplay(controller, menu);
+                        }
+                        else
+                        {
+                            Program.ConsoleCenter("Please enter valid command // ");
+                        }
+                    }
+                }
+                // Check win
+                if (controller.CheckWin(player1.Turn))
+                {
+                    Console.Clear();
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.CursorLeft = leftMargin;
+                    Console.WriteLine("CONGRATS {0} You Won !", player1.Name);
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    while (true)
+                    {
+                        Program.ConsoleCenter("Enter X to go to menu : ");
+                        string input = Console.ReadLine();
+                        if (input.ToLower() == "x")
+                        {
+                            menu.MenuDisplay(controller, menu);
+                        }
+                        else
+                        {
+                            Program.ConsoleCenter("Please enter valid command // ");
+                        }
+                    }
+
+                }
+                if (controller.CheckWin(player2.Turn))
+                {
+                    Console.Clear();
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.CursorLeft = leftMargin;
+                    Console.WriteLine("CONGRATS {0} You Won !", player2.Name);
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    while (true)
+                    {
+                        Program.ConsoleCenter("Enter X to go to menu : ");
+                        string input = Console.ReadLine();
+                        if (input.ToLower() == "x")
+                        {
+                            menu.MenuDisplay(controller, menu);
+                        }
+                        else
+                        {
+                            Program.ConsoleCenter("Please enter valid command // ");
+                        }
+                    }
+                }
+                Console.WriteLine(" ");
+
+                while (i == 0)
+                {
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.CursorLeft = centerMargin;
+                    Console.Write("Your turn {0} (Choose a number) : ", player1.Name);
+                    string input = Console.ReadLine();
+                    if (string.IsNullOrWhiteSpace(input))
+                    {
+
+                        Program.ConsoleCenter("Please enter a valid number.");
+                        continue;
+                    }
+                    if (int.TryParse(input, out selectedCol))
+                    {
+                        if (selectedCol >= 1 && selectedCol <= 7)
+                        {
+                            i = 1;
+                            Console.WriteLine(" ");
+                            controller.ChangeBoard(selectedCol, player1.Turn);
+                        }
+                        else
+                        {
+                            Program.ConsoleCenter("Please enter a number between 1 and 7.");
+                        }
+                    }
+                    else
+                    {
+                        Program.ConsoleCenter("Please enter a valid number.");
+                    }
+                }
+
+
+
+                Console.Clear();
+                // Player 2 turn
+                controller.GameBoardDisplay();
+                // Check draw
+                if (controller.IsFull())
+                {
+                    Console.Clear();
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.CursorLeft = leftMargin;
+                    Console.WriteLine("Its a DRAW guys ! play again //");
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    while (true)
+                    {
+                        Program.ConsoleCenter("Enter X to go to menu : ");
+                        string input = Console.ReadLine();
+                        if (input.ToLower() == "x")
+                        {
+                            menu.MenuDisplay(controller, menu);
+                        }
+                        else
+                        {
+                            Program.ConsoleCenter("Please enter valid command // ");
+                        }
+                    }
+                }
+                // Check win
+                if (controller.CheckWin(player1.Turn))
+                {
+                    Console.Clear();
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.CursorLeft = leftMargin;
+                    Console.WriteLine("CONGRATS {0} You Won !", player1.Name);
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    while (true)
+                    {
+                        Program.ConsoleCenter("Enter X to go to menu : ");
+                        string input = Console.ReadLine();
+                        if (input.ToLower() == "x")
+                        {
+                            menu.MenuDisplay(controller, menu);
+                        }
+                        else
+                        {
+                            Program.ConsoleCenter("Please enter valid command // ");
+                        }
+                    }
+                }
+                if (controller.CheckWin(player2.Turn))
+                {
+                    Console.Clear();
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.CursorLeft = leftMargin;
+                    Console.WriteLine("CONGRATS {0} You Won !", player2.Name);
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    while (true)
+                    {
+                        Program.ConsoleCenter("Enter X to go to menu : ");
+                        string input = Console.ReadLine();
+                        if (input.ToLower() == "x")
+                        {
+                            menu.MenuDisplay(controller, menu);
+                        }
+                        else
+                        {
+                            Program.ConsoleCenter("Please enter valid command // ");
+                        }
+                    }
+                }
+                Console.WriteLine(" ");
+                while (i == 1)
+                {
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.CursorLeft = centerMargin;
+                    Console.Write("Its now AI turn : ");
+
+                    controller.AiLogic(controller);
+                    i = 0;
+                    Thread.Sleep(2000);
+
+                }
+
+
+            } while (exit == 0);
+        }
+
+        public void AiLogic(Controller controller)
+        {
+            Random rand = new Random();
+            int selectedCol = rand.Next(1, 8);
+            controller.ChangeBoard(selectedCol, player2.Turn);
+        }
+
+    }
+}
---
+++ Menu.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Connect4_Final_Ptoject
+{
+    internal class Menu
+    {
+        public void MenuDisplay(Controller controller, Menu menu)
+        {
+            Console.Clear();
+            var leftMargin = (Console.WindowWidth - 11) / 2;
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.CursorLeft = leftMargin;
+            Program.typeWrite("CONNECT FOUR");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+
+            Program.ConsoleCenter("1. Play Game ( Player vs Player ) ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Program.ConsoleCenter("2. Play Game ( Player vs AI ) ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Program.ConsoleCenter("3. Exit ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+
+
+
+            while (true)
+            {
+                string inputStr;
+                while (true)
+                {
+                    Program.ConsoleCenter("(Enter as number) => ");
+                    inputStr = Console.ReadLine();
+                    if (string.IsNullOrWhiteSpace(inputStr))
+                    {
+                        Program.ConsoleCenter("// Invalid input //");
+                        Console.WriteLine("");
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                
+                Console.WriteLine(" ");
+                if (inputStr == "1")
+                {
+                    controller.ResetBoard();
+                    controller.PlayGame(controller, menu);
+
+                }
+                else if (inputStr == "2")
+                {
+                    Console.Clear();
+                    controller.ResetBoard();
+                    controller.AiPlayGame(controller, menu);
+                }
+                else if (inputStr == "3")
+                {
+                    Console.Clear();
+                    Program.ConsoleCenter(" Are you sure you want to quit ?");
+                    Console.WriteLine(" ");
+                    Program.ConsoleCenter("Press ENTER key to go to menu or X to quit : ");
+                    string temp = Console.ReadLine();
+                    if (temp.ToLower() == "x")
+                    {
+                        Environment.Exit(0);
+                    }
+                    MenuDisplay(controller, menu);
+                }
+            }
+        }
+
+    }
+
+}
+
