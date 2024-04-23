2ee2c47a5b0c1c525aa0916ae0b384816f09ae5c
Deepak Poly <119725894+Daddy2424@users.noreply.github.com>
2024-04-24 03:17:28 +0530

    Update GameBoard.cs

--- GameBoard.cs
+++ GameBoard.cs
@@ -1,12 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Connect4_Final_Ptoject
 {
-    internal class GameBoard
+    internal class GameBoard : Player
     {
         private int _rows;
         private int _columns;
@@ -45,8 +46,21 @@ namespace Connect4_Final_Ptoject
 
         public void GameBoardDisplay()
         {
+            Console.WriteLine(" ");
+            Program.ConsoleCenter("CONNECT FOUR");
+            Console.WriteLine(" ");
+            var leftMargin = (Console.WindowWidth - 11) / 2;
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
             for (int i = 0; i < _rows; i++)
             {
+                Console.CursorLeft = leftMargin;
                 for (int j = 0; j < _columns; j++)
                 {
                     Console.Write(_arr[i, j]);
@@ -54,6 +68,8 @@ namespace Connect4_Final_Ptoject
                 }
                 Console.WriteLine("");
             }
+
+            Console.CursorLeft = leftMargin;
             for (int i = 1; i < _rows + 2; i++)
             {
                 Console.Write(i);
@@ -61,9 +77,394 @@ namespace Connect4_Final_Ptoject
             }
         }
 
-     
+        public void ChangeBoard(int selectedCol, string playerTurn)
+        {
+            if (selectedCol >= 1 && selectedCol <= _columns)
+            {
+                int row = _rows - 1;
+                while (row >= 0)
+                {
+                    if (_arr[row, selectedCol - 1] == "-")
+                    {
+                        _arr[row, selectedCol - 1] = playerTurn;
+                        return;
+                    }
+                    row--;
+                }
+            }
+            Console.WriteLine("Column is full! no chance no more...");
+            Thread.Sleep(2000);
+        }
 
-        
+        public void PlayGame(Controller controller,Menu menu)
+        {
+            Console.Clear();
+            var originalMargin = (Console.WindowWidth - 6) / 2;
+            var leftMargin = (Console.WindowWidth - 21) / 2;
+            var centerMargin = (Console.WindowWidth - 31) / 2;
+            while (true)
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine(" ");
+                Program.ConsoleCenter("Enter Player 1 name : ");
+                string player1Name = Console.ReadLine();
+                Console.WriteLine(" ");
+                Program.ConsoleCenter("Enter Player 2 name : ");
+                string player2Name = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(player1Name) || string.IsNullOrWhiteSpace(player2Name))
+                {
+                    Console.Clear();
+                    Program.ConsoleCenter("Please enter a valid name // ");
+                    Thread.Sleep(2000);
+                    Console.Clear();
+                }
+                else
+                {
+                    CreatePlayer(player1Name, player2Name);
+                    break;
+                }
+            }
+            Console.Clear();
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Program.ConsoleCenter("Player 1 will be X and Player 2 will be O ");
+            Console.WriteLine(" ");
+            Console.CursorLeft = originalMargin;
+            Program.typeWrite("...." , 1000);
+            Thread.Sleep(1000);
+
+            int exit = 0;
+            int i = 0;
+            int selectedCol;
+            do
+            {
+                Console.Clear();
+                // Player 1 turn
+                controller.GameBoardDisplay();
+                // check draw
+                if (controller.IsFull())
+                {
+                    Console.Clear();
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.CursorLeft = leftMargin;
+                    Console.WriteLine("Its a DRAW guys ! play again //");
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    while (true)
+                    {
+                        Program.ConsoleCenter("Enter X to go to menu : ");
+                        string input = Console.ReadLine();
+                        if (input.ToLower() == "x")
+                        {
+                            menu.MenuDisplay(controller, menu);
+                        }
+                        else
+                        {
+                            Program.ConsoleCenter("Please enter valid command // ");
+                        }
+                    }
+                }
+                // Check win
+                if (controller.CheckWin(player1.Turn))
+                {
+                    Console.Clear();
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.CursorLeft = leftMargin ;
+                    Console.WriteLine("CONGRATS {0} You Won !", player1.Name);
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    while (true)
+                    {
+                        Program.ConsoleCenter("Enter X to go to menu : ");
+                        string input = Console.ReadLine();
+                        if (input.ToLower() == "x")
+                        {
+                            menu.MenuDisplay(controller, menu);
+                        }
+                        else
+                        {
+                            Program.ConsoleCenter("Please enter valid command // ");
+                        }
+                    }
+                    
+                }
+                if (controller.CheckWin(player2.Turn))
+                {
+                    Console.Clear();
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.CursorLeft = leftMargin;
+                    Console.WriteLine("CONGRATS {0} You Won !", player2.Name);
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    while (true)
+                    {
+                        Program.ConsoleCenter("Enter X to go to menu : ");
+                        string input = Console.ReadLine();
+                        if (input.ToLower() == "x")
+                        {
+                            menu.MenuDisplay(controller, menu);
+                        }
+                        else
+                        {
+                            Program.ConsoleCenter("Please enter valid command // ");
+                        }
+                    }
+                }
+                Console.WriteLine(" ");
+
+                while (i == 0)
+                {
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.CursorLeft = centerMargin;
+                    Console.Write("Your turn {0} (Choose a number) : ", player1.Name);
+                    string input = Console.ReadLine();
+                    if (string.IsNullOrWhiteSpace(input))
+                    {
+
+                        Program.ConsoleCenter("Please enter a valid number.");
+                        continue;
+                    }
+                    if (int.TryParse(input, out selectedCol))
+                    {
+                        if (selectedCol >= 1 && selectedCol <= 7)
+                        {
+                            i = 1;
+                            Console.WriteLine(" ");
+                            controller.ChangeBoard(selectedCol, player1.Turn);
+                        }
+                        else
+                        {
+                            Program.ConsoleCenter("Please enter a number between 1 and 7.");
+                        }
+                    }
+                    else
+                    {
+                        Program.ConsoleCenter("Please enter a valid number.");
+                    }
+                }
+
+
+
+                Console.Clear();
+                // Player 2 turn
+                controller.GameBoardDisplay();
+                // Check draw
+                if (controller.IsFull())
+                {
+                    Console.Clear();
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.CursorLeft = leftMargin;
+                    Console.WriteLine("Its a DRAW guys ! play again //");
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    while (true)
+                    {
+                        Program.ConsoleCenter("Enter X to go to menu : ");
+                        string input = Console.ReadLine();
+                        if (input.ToLower() == "x")
+                        {
+                            menu.MenuDisplay(controller, menu);
+                        }
+                        else
+                        {
+                            Program.ConsoleCenter("Please enter valid command // ");
+                        }
+                    }
+                }
+                // Check win
+                if (controller.CheckWin(player1.Turn))
+                {
+                    Console.Clear();
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.CursorLeft = leftMargin;
+                    Console.WriteLine("CONGRATS {0} You Won !", player1.Name);
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    while (true)
+                    {
+                        Program.ConsoleCenter("Enter X to go to menu : ");
+                        string input = Console.ReadLine();
+                        if (input.ToLower() == "x")
+                        {
+                            menu.MenuDisplay(controller, menu);
+                        }
+                        else
+                        {
+                            Program.ConsoleCenter("Please enter valid command // ");
+                        }
+                    }
+                }
+                if (controller.CheckWin(player2.Turn))
+                {
+                    Console.Clear();
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.CursorLeft = leftMargin;
+                    Console.WriteLine("CONGRATS {0} You Won !", player2.Name);
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    while (true)
+                    {
+                        Program.ConsoleCenter("Enter X to go to menu : ");
+                        string input = Console.ReadLine();
+                        if (input.ToLower() == "x")
+                        {
+                            menu.MenuDisplay(controller, menu);
+                        }
+                        else
+                        {
+                            Program.ConsoleCenter("Please enter valid command // ");
+                        }
+                    }
+                }
+                Console.WriteLine(" ");
+                while (i == 1)
+                {
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.CursorLeft = centerMargin;
+                    Console.Write("Your turn {0} (Choose a number) : ", player2.Name);
+                    string input = Console.ReadLine();
+                    if (string.IsNullOrWhiteSpace(input))
+                    {
+                        Program.ConsoleCenter("Please enter a valid number.");
+                        continue;
+                    }
+                    if (int.TryParse(input, out selectedCol))
+                    {
+                        if (selectedCol >= 1 && selectedCol <= 7)
+                        {
+                            i = 0;
+                            Console.WriteLine(" ");
+                            controller.ChangeBoard(selectedCol, player2.Turn);
+                        }
+                        else
+                        {
+                            Program.ConsoleCenter("Please enter a number between 1 and 7.");
+                        }
+                    }
+                    else
+                    {
+                        Program.ConsoleCenter("Please enter a valid number.");
+                    }
+                }
+
+
+            } while (exit == 0);
+        }
+
+         public bool CheckWin(string playerTurn)
+        {
+            // check horizontal wins
+            for (int i = 0; i < _rows; i++)
+            {
+                for (int j = 0; j < _columns - 3; j++)
+                {
+                    if ((_arr[i, j]) == playerTurn &&
+                        _arr[i, j + 1] == playerTurn &&
+                        _arr[i, j + 2] == playerTurn &&
+                        _arr[i, j + 3] == playerTurn)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            // Check vertical wins
+            for (int i = 0; i < _columns; i++)
+            {
+                for (int j = 0; j < _rows - 3; j++)
+                {
+                    if ((_arr[j, i]) == playerTurn &&
+                        _arr[j + 1, i] == playerTurn &&
+                        _arr[j + 2, i] == playerTurn &&
+                        _arr[j + 3, i] == playerTurn)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            // check diagnol wins
+            for (int i = 0; i < _rows - 3; i++)
+            {
+                for (int j = 0; j < _columns - 3; j++)
+                {
+                    if ((_arr[i, j]) == playerTurn &&
+                        _arr[i + 1, j + 1] == playerTurn &&
+                        _arr[i + 2, j + 2] == playerTurn &&
+                        _arr[i + 3, j + 3] == playerTurn)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            for (int i = 0; i < _rows - 3; i++)
+            {
+                for (int j = _columns - 1; j >= 3; j--)
+                {
+                    if ((_arr[i, j]) == playerTurn &&
+                        _arr[i + 1, j - 1] == playerTurn &&
+                        _arr[i + 2, j - 2] == playerTurn &&
+                        _arr[i + 3, j - 3] == playerTurn)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+       
+        public bool IsFull()
+        {
+            for (int i = 0; i < _rows; i++)
+            {
+                for (int j = 0; j < _columns; j++)
+                {
+                    if(_arr[i, j] == "-")
+                    {
+                        return false;
+                    }
+                    
+                }
+            }
+            return true;
+        }
+
+        public void ResetBoard()
+        {
+            for (int i = 0; i < _rows; i++)
+            {
+                for (int j = 0; j < _columns; j++)
+                {
+                    _arr[i, j] = "-";
+                }
+            }
+        }
     }
 
+        
 }
+
+
