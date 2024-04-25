69f7e9447f131890cb5d811a15b5dfd05380388f
Deepak Poly <119725894+Daddy2424@users.noreply.github.com>
2024-04-24 22:28:13 -0600

    Add files via upload

---
+++ Credits.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Connect4_Final_Ptoject
+{
+    internal class Credits
+    {
+        public Credits() { }
+
+        // i loved working on this credits page !!
+        public void ShowCredits()
+        {
+            Sounds s = new Sounds();
+            
+            var leftMargin = (Console.WindowWidth - 31) / 2;
+            Thread.Sleep(2000);
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.CursorLeft = leftMargin;
+            Program.typeWrite("PIXELPULSE Studios presents");
+            Thread.Sleep(100);
+            s.PlayIntro();
+            Thread.Sleep(3500);
+            Console.Clear();
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.CursorLeft = leftMargin;
+            Console.WriteLine(@"
+                                                ____ ___  _   _ _   _ _____ ____ _____   _____ ___  _   _ ____  
+                                               / ___/ _ \| \ | | \ | | ____/ ___|_   _| |  ___/ _ \| | | |  _ \ 
+                                              | |  | | | |  \| |  \| |  _|| |     | |   | |_ | | | | | | | |_) |
+                                              | |__| |_| | |\  | |\  | |__| |___  | |   |  _|| |_| | |_| |  _ < 
+                                               \____\___/|_| \_|_| \_|_____\____| |_|   |_|   \___/ \___/|_| \_\
+                                                                                                                                       
+");
+            Thread.Sleep(1500);
+            Console.CursorLeft = leftMargin;
+            Program.typeWrite("By Deepak Poly");
+            Thread.Sleep(2500);
+            Console.CursorLeft = leftMargin;
+            Console.Write("@deepakpoly1980@gmail.com");
+            Thread.Sleep(2000);
+            Console.CursorLeft = leftMargin;
+            Console.Write("                         ");
+            Console.CursorLeft = leftMargin;
+            Console.Write("Play with your friends and family");
+            Thread.Sleep(2000);
+            Console.CursorLeft = leftMargin;
+            Console.Write("                                 ");
+            Console.CursorLeft = leftMargin;
+            Console.Write("Enjoy Guys!");
+            Thread.Sleep(2000);
+
+        }
+
+
+
+    }
+}
---
+++ Sounds.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect4_Final_Ptoject
+{
+    // Sounds (it was funn!!)
+    internal class Sounds
+    {
+        string BgmSoundPath = @"./sounds/bgm.wav";
+        string IntroPath = @"./sounds/intro.wav";
+        string MatchSoundPath = @"./sounds/match.wav";
+
+        public void PlayBgm()
+        {
+            SoundPlayer player = new SoundPlayer(BgmSoundPath);
+            player.Play();
+        }
+        public void StopBgm()
+        {
+            SoundPlayer soundPlayer = new SoundPlayer(BgmSoundPath);
+            soundPlayer.Stop();
+        }
+        public void PlayIntro()
+        {
+            SoundPlayer playPlayer = new SoundPlayer(IntroPath);
+            playPlayer.Play();
+        }
+        
+        public void PlayMatch() 
+        {
+            SoundPlayer matchPlayer = new SoundPlayer(MatchSoundPath);
+            matchPlayer.Play();
+        }
+
+        public void StopMatch()
+        {
+            SoundPlayer playPlayer = new SoundPlayer(MatchSoundPath);
+            playPlayer.Stop();
+        }
+
+    }
+}
